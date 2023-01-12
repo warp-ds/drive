8e507e6b7ffed6094b1ba3a282ea61dcbade593d
Dave Honneffer <pearofducks@gmail.com>
2023-01-12 20:09:18 +0100

    Wire the rest of breakpoint variant

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // import { preflights } from './preflights.js'
 import { rules } from './rules.js'
 import { variants } from './variants.js'
-// import { theme } from './theme.js'
+import { theme } from './theme.js'
 
 // export { theme, colors } from './theme.js'
 
 // TODO: improve generic type passed here
 /** @type {import('@unocss/core').Preset<object>} */
 export function presetEngine (options = {}) {
-
   return {
     name: '@nmp-ds/engine',
+    theme,
     rules,
     variants,
   }
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const breakpoints = {
+  // sm hits typical mobiles in landscape and up
+  sm: '480px',
+  // md hits tablets and up
+  md: '768px',
+  // lg hits full desktop-width and up
+  lg: '990px',
+}
+
+export const theme = {
+  breakpoints
+}
