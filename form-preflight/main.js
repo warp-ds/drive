d00195c07c0200c600f7098b16a80ee9d5561633
Dave Honneffer <pearofducks@gmail.com>
2023-01-16 16:22:45 +0100

    Scaffold the form dev env in

---
+++ form-preflight/main.js
@@ -0,0 +1,5 @@
+// @unocss-include
+import './tw-reset.css' // this can probably go away once tw-reset is a part of the plugin
+import 'uno.css'
+import './forms.css'
+import './formVariables.js'
---
+++ form-preflight/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import { presetEngine } from '@nmp-ds/engine'
+
+export default defineConfig({
+  plugins: [
+    UnoCSS({ presets: [presetEngine()] })
+  ]
+})
