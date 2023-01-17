3d78d1fdf3ceeaa79ffe8aa9a7dfbaa7231fb78f
Dave Honneffer <pearofducks@gmail.com>
2023-01-17 13:38:10 +0100

    Wire up tw-reset preflight (#2)

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,22 +1,25 @@
 // import { preflights } from './preflights.js'
 import { formPreflight } from '@nmp-ds/form-preflight'
+import { twReset } from './_preflights/tw-reset.js'
 import { rules } from '#rules'
 import { variants } from '#variants'
 import { theme } from './theme.js'
+
+const includePreflight = ['base', 'hyper']
 
 // export { theme, colors } from './theme.js'
 
 // TODO: improve generic type passed here
 /** @type {import('@unocss/core').Preset<object>} */
 export function presetEngine (options = {}) {
+  const mode = options.mode ?? 'app'
+  const hasPreflight = includePreflight.includes(mode)
   return {
     name: '@nmp-ds/engine',
     theme,
     rules,
     variants,
-    preflights: [
-      formPreflight
-    ]
+    preflights: hasPreflight ? [twReset, formPreflight] : []
   }
 }
 
