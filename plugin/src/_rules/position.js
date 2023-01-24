955580d2b5a0ba2f86df9889cfd0df33c3e178d7
BalbinaK <balbuhhha@gmail.com>
2023-01-24 16:14:06 +0100

    Add rules for inset and top, left, right bottom classes

---
+++ plugin/src/_rules/position.js
@@ -0,0 +1,24 @@
+import { handler as h, insetMap } from '#utils';
+
+function handleInsetValue(v, { theme }) {
+    return theme.spacing?.[v] ?? h.fraction.auto(v); // TODO: warn if no value
+}
+function handleInsetValues([, d, v], ctx) {
+    const r = handleInsetValue(v, ctx);
+    if (r != null && d in insetMap)
+        return insetMap[d].map(i => [i.slice(1), r]);
+}
+export const insets = [
+    [/^inset-(.+)$/, ([, v], ctx) => ({ inset: handleInsetValue(v, ctx) }),
+        {
+            autocomplete: [
+                '(inset-<directions>-$spacing',
+                'inset-(block|inline)-$spacing',
+                'inset-(bs|be|is|ie)-$spacing',
+                '(top|left|right|bottom)-$spacing',
+            ],
+        },
+    ],
+    [/^inset-([xy])-(.+)$/, handleInsetValues],
+    [/^(top|left|right|bottom)-(.+)$/, ([, d, v], ctx) => ({ [d]: handleInsetValue(v, ctx) })],
+];
