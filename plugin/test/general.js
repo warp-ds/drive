f0937876d09968af10fc7df9de1dbdc4a827ca67
BalbinaK <balbuhhha@gmail.com>
2023-01-30 17:07:14 +0100

    Update staticClasses length value to pass general.js tests

--- plugin/test/general.js
+++ plugin/test/general.js
@@ -10,7 +10,7 @@ test('all static rules generate', async ({ uno }) => {
   const generated = await uno.generate(staticClasses)
   // generated.matched is a Set
   assert.equal(generated.matched.size, staticClasses.length)
-  assert.equal(staticClasses.length, 95)
+  assert.equal(staticClasses.length, 165)
 })
 
 test('display rules are sane', async ({ uno }) => {
