9ad2a38c34571356949009d7b6596f59f2dae1aa
Dave Honneffer <pearofducks@gmail.com>
2023-02-04 08:26:29 +0100

    Rework project structure, get rid of monorepo (#24)

--- form-preflight/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import UnoCSS from 'unocss/vite'
-import { presetEngine } from '@warp-ds/uno'
-
-export default defineConfig({
-  plugins: [
-    UnoCSS({ presets: [presetEngine({ mode: 'hyper' })] })
-  ]
-})
--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,4 @@
-import { formPreflight } from '@warp-ds/form-preflight'
-import { twReset } from './_preflights/tw-reset.js'
+import { preflights } from '#preflights'
 import { rules } from '#rules'
 import { variants } from '#variants'
 import { useTheme } from '#theme'
@@ -18,14 +17,14 @@ const includePreflight = ['base', 'hyper']
 /** @type {import('@unocss/core').Preset<object>} */
 export function presetWarp (options = {}) {
   const mode = options.mode ?? 'app'
-  const hasPreflight = includePreflight.includes(mode)
+  const hasPreflight = options.usePreflight ?? includePreflight.includes(mode)
   const theme = useTheme(options)
   return {
     name: '@nmp-ds/engine',
     theme,
     rules,
     variants,
-    preflights: hasPreflight ? [twReset, formPreflight] : []
+    preflights: hasPreflight ? preflights : []
   }
 }
 
