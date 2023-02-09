e2d673e6bd3b55f9cd3334d1922af9dcae785a4a
Dave Honneffer <pearofducks@gmail.com>
2023-02-09 09:31:41 +0100

    Ship a client build because rollup hasn't figured out it's 2023

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 export default defineConfig({
   input: './src/plugin.js',
   output: {
-    file: './dist/engine.js',
+    file: './dist/drive.js',
     format: 'esm'
   },
   plugins: [nodeResolve()]
