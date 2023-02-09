d1ea3fb45e57c8e5ef0e1e8e5cf950f89feeb563
Niklas Wikstrand Nordahl <niklas.wikstrand@gmail.com>
2023-02-09 14:16:25 +0100

    feat: add rules for table (#33)

---
+++ src/_rules/table.js
@@ -0,0 +1,36 @@
+export const borderCollapse = [
+  ["border-collapse", { 'border-collapse': "collapse" }],
+  ["border-separate", { 'border-separate': "separate" }]
+]
+
+export const tableLayout = [
+  ["table-auto", { 'table-layout': "auto" }],
+  ["table-fixed", { 'table-layout': "fixed" }]
+]
+
+export const borderSpacing = [
+  [/^border-spacing-()(\d+)?$/, handleBorderSpacing],
+  [/^border-spacing-([xy])-(\d+)?$/, handleBorderSpacing]
+]
+
+function handleBorderSpacing([, direction, spacing], { theme }) {
+  const spacingValue = theme.spacing?.[spacing];
+  const defaultValue = theme.spacing[1];
+
+  if(!Boolean(spacingValue)) return;
+
+  const borderSpacingValue = () => {
+    switch(direction) {
+      case "x":
+        return `${spacingValue} ${defaultValue}`;
+      case "y":
+        return `${defaultValue} ${spacingValue}`;
+      default:
+        return `${spacingValue} ${spacingValue}`;
+    }
+  }
+
+  return {
+    "border-spacing": borderSpacingValue()
+  }
+}
---
+++ test/table.js
@@ -0,0 +1,39 @@
+import { expect, test } from 'vitest'
+import { spaceBase } from '#theme';
+import { setup } from './_helpers.js'
+
+setup()
+
+test('border-collapse', async (t) => {
+  const classes = ["border-collapse", "border-separate"]
+
+  const { css } = await t.uno.generate(classes)
+
+  expect(css).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .border-collapse{border-collapse:collapse;}
+    .border-separate{border-separate:separate;}"
+  `)
+})
+
+test('border-collapse', async (t) => {
+  const classes = ["table-auto", "table-fixed"]
+
+  const { css } = await t.uno.generate(classes)
+
+  expect(css).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .table-auto{table-layout:auto;}
+    .table-fixed{table-layout:fixed;}"
+  `)
+})
+
+test('border-spacing', async (t) => {
+  const classes = spaceBase.map((spacingUnit) => {
+    return [`border-spacing-x-${spacingUnit}`, `border-spacing-y-${spacingUnit}`, `border-spacing-${spacingUnit}`];
+  }).flat()
+
+  const { css } = await t.uno.generate(classes)
+
+  expect(css).toMatchSnapshot()
+})
