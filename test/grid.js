e464bb221f4dbf5456907b70a7f9e7f1d547ee51
Anna Rybkina <anna.rybkina@schibsted.com>
2023-02-10 08:55:39 +0100

    More tests

--- test/grid.js
+++ test/grid.js
@@ -1,6 +1,6 @@
-import { setup } from './_helpers.js';
 import { expect, assert, test } from 'vitest';
-import { gridCol, gridRow } from '../src/bounds.js';
+import { gridCol, gridRow } from '#bounds';
+import { setup } from './_helpers.js';
 
 setup();
 
@@ -18,11 +18,11 @@ test('grid span', async (t) => {
     'row-span-full',
   ];
 
-  const artbitraryClassesRows = rows.map((_, i) => {
-    return `row-span-${i + 1}`;
+  const artbitraryClassesRows = rows.map((num) => {
+    return `row-span-${num}`;
   });
-  const artbitraryClassesCols = columns.map((_, i) => {
-    return `col-span-${i + 1}`;
+  const artbitraryClassesCols = columns.map((num) => {
+    return `col-span-${num}`;
   });
 
   const { css } = await t.uno.generate([
@@ -35,15 +35,15 @@ test('grid span', async (t) => {
 });
 
 test('grid starts and ends', async (t) => {
-  const classes = ['row-start-1', 'row-end-5', 'col-start-1', 'col-end-3'];
-  const { css } = await t.uno.generate(classes);
-  expect(css).toMatchInlineSnapshot(`
-    "/* layer: default */
-    .row-start-1{grid-row-start:1;}
-    .col-start-1{grid-column-start:1;}
-    .row-end-5{grid-row-end:5;}
-    .col-end-3{grid-column-end:3;}"
-  `);
+  const classesRows = rows.map((num) => {
+    return [`row-start-${num}`, `row-end-${num}`];
+  });
+  const classesCols = columns.map((num) => {
+    return [`col-start-${num}`, `col-end-${num}`];
+  });
+
+  const { css } = await t.uno.generate([...classesRows, ...classesCols].flat());
+  expect(css).toMatchSnapshot();
 });
 
 test('grid columns with arbitrary values', async (t) => {
@@ -79,36 +79,34 @@ test('grid auto flows', async (t) => {
   ];
   const { css } = await t.uno.generate(gridFlows);
 
-  expect(css).toMatchInlineSnapshot(`
-    "/* layer: default */
-    .auto-rows-auto{grid-auto-rows:auto;}
-    .auto-rows-fr{grid-auto-rows:minmax(0,1fr);}
-    .auto-rows-max{grid-auto-rows:max-content;}
-    .auto-rows-min{grid-auto-rows:min-content;}
-    .grid-flow-row{grid-auto-flow:row;}
-    .grid-flow-col{grid-auto-flow:column;}
-    .grid-flow-dense{grid-auto-flow:dense;}
-    .grid-flow-row-dense{grid-auto-flow:row dense;}
-    .grid-flow-col-dense{grid-auto-flow:column dense;}"
-  `);
+  expect(css).toMatchSnapshot();
 });
 
 test('grid templates basic', async (t) => {
-  const numbers = Array.from({ length: 5 });
-
-  const classesRows = rows.map((_, i) => {
-    return `grid-rows-${i + 1}`;
+  const classesRows = rows.map((num) => {
+    return `grid-rows-${num}`;
   });
-  const classesCols = columns.map((_, i) => {
-    return `grid-cols-${i + 1}`;
+  const classesCols = columns.map((num) => {
+    return `grid-cols-${num}`;
   });
 
   const { css } = await t.uno.generate([...classesCols, ...classesRows]);
   expect(css).toMatchSnapshot();
 });
 
-test('grid templates do not render if out of range', async (t) => {
-  const classes = ['grid-rows-8', 'grid-rows-13'];
+test('grid styling is not created if out of range', async (t) => {
+  const outOfRangeRow = rows.length + 1;
+  const outOfRangeColumn = columns.length + 1;
+  const classes = [
+    `grid-rows-${outOfRangeRow}`,
+    `grid-columns-${outOfRangeColumn}`,
+    `row-span-${outOfRangeRow}`,
+    `col-span-${outOfRangeColumn}`,
+    `col-start-${outOfRangeColumn}`,
+    `col-end-${outOfRangeColumn}`,
+    `row-start-${outOfRangeRow}`,
+    `row-end-${outOfRangeRow}`,
+  ];
   const { css } = await t.uno.generate(classes);
 
   expect(css).toMatchInlineSnapshot('""');
@@ -118,9 +116,5 @@ test('grid templates none', async (t) => {
   const classes = ['grid-rows-none', 'grid-cols-none'];
   const { css } = await t.uno.generate(classes);
 
-  expect(css).toMatchInlineSnapshot(`
-    "/* layer: default */
-    .grid-rows-none{grid-template-rows:none;}
-    .grid-cols-none{grid-template-columns:none;}"
-  `);
+  expect(css).toMatchSnapshot();
 });
