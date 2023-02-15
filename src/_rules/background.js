c652ff0075c8fb30f409494a958869ea3f3a449c
Dave Honneffer <pearofducks@gmail.com>
2023-02-15 13:05:23 +0100

    Add background rules

---
+++ src/_rules/background.js
@@ -0,0 +1,39 @@
+import { positionMap, globalKeywords, makeGlobalStaticRules } from '#utils'
+
+export const backgrounds = [
+  // size
+  ['bg-auto', { 'background-size': 'auto' }],
+  ['bg-cover', { 'background-size': 'cover' }],
+  ['bg-contain', { 'background-size': 'contain' }],
+  // attachments
+  ['bg-fixed', { 'background-attachment': 'fixed' }],
+  ['bg-local', { 'background-attachment': 'local' }],
+  ['bg-scroll', { 'background-attachment': 'scroll' }],
+  // positions
+  // skip 1 & 2 letters shortcut
+  [/^bg-([-\w]{3,})$/, ([, s]) => ({ 'background-position': positionMap[s] })],
+  [/^bg-([-\w]{3,})-([-\w]{3,})$/, ([, first, second]) => ({ 'background-position': `${positionMap[first]} ${positionMap[second]}` })],
+  // clip
+  ['bg-clip-border', { '-webkit-background-clip': 'border-box', 'background-clip': 'border-box' }],
+  ['bg-clip-content', { '-webkit-background-clip': 'content-box', 'background-clip': 'content-box' }],
+  ['bg-clip-padding', { '-webkit-background-clip': 'padding-box', 'background-clip': 'padding-box' }],
+  ['bg-clip-text', { '-webkit-background-clip': 'text', 'background-clip': 'text' }],
+  ...globalKeywords.map(keyword => [`bg-clip-${keyword}`, {
+    '-webkit-background-clip': keyword,
+    'background-clip': keyword,
+  }]),
+  // repeats
+  ['bg-repeat', { 'background-repeat': 'repeat' }],
+  ['bg-no-repeat', { 'background-repeat': 'no-repeat' }],
+  ['bg-repeat-x', { 'background-repeat': 'repeat-x' }],
+  ['bg-repeat-y', { 'background-repeat': 'repeat-y' }],
+  ['bg-repeat-round', { 'background-repeat': 'round' }],
+  ['bg-repeat-space', { 'background-repeat': 'space' }],
+  ...makeGlobalStaticRules('bg-repeat', 'background-repeat'),
+
+  // origins
+  ['bg-origin-border', { 'background-origin': 'border-box' }],
+  ['bg-origin-padding', { 'background-origin': 'padding-box' }],
+  ['bg-origin-content', { 'background-origin': 'content-box' }],
+  ...makeGlobalStaticRules('bg-origin', 'background-origin'),
+]
---
+++ test/background.js
@@ -0,0 +1,39 @@
+import { setup } from './_helpers.js'
+import { expect, test } from 'vitest'
+import { positionMap } from '#utils'
+
+setup()
+
+test('bg positions', async ({ uno }) => {
+  const positions = Object.values(positionMap)
+  const classes = positions.map(e => `bg-${e}`)
+  const twoDimensionClasses = positions.flatMap(e => {
+    return positions.map(f => `bg-${e}-${f}`)
+  })
+  const { css } = await uno.generate([...classes, ...twoDimensionClasses])
+  expect(css).toMatchSnapshot()
+})
+
+test('bg repeats', async ({ uno }) => {
+  const classes = ['repeat', 'no-repeat', 'repeat-x', 'repeat-y', 'repeat-round', 'repeat-space'].map(e => `bg-${e}`)
+  const { css } = await uno.generate(classes)
+  expect(css).toMatchSnapshot()
+})
+
+test('bg origins', async ({ uno }) => {
+  const classes = ['border', 'padding', 'content'].map(e => `bg-origin-${e}`)
+  const { css } = await uno.generate(classes)
+  expect(css).toMatchSnapshot()
+})
+
+test('bg size', async ({ uno }) => {
+  const classes = ['auto', 'cover', 'contain'].map(e => `bg-${e}`)
+  const { css } = await uno.generate(classes)
+  expect(css).toMatchSnapshot()
+})
+
+test('bg attachments', async ({ uno }) => {
+  const classes = ['fixed', 'local', 'scroll'].map(e => `bg-${e}`)
+  const { css } = await uno.generate(classes)
+  expect(css).toMatchSnapshot()
+})
