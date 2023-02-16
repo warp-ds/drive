bb82e22a5adf5886846ceab34d3a059c8a52acb0
Anna Rybkina <anna.rybkina@anicode.se>
2023-02-16 09:57:55 +0100

    Add tokes for text and bg color

--- src/_rules/color.js
+++ src/_rules/color.js
@@ -1,3 +1,15 @@
 export const opacity = [
   [/^opacity-?(\d+)$/, ([, d], { theme }) => ({ opacity: theme.opacity[d] }), { autocomplete: 'opacity-${opacity}' }],
 ];
+
+export const textColors = [
+  ['text', { color: 'var(--w-text-color)' }],
+  ['text-inverted', { color: 'var(--w-text-color-inverted)' }, { autocomplete: 'text-inverted' }],
+  ['text-inverted-subtle', { color: 'var(--w-text-color-inverted-subtle)' }, { autocomplete: 'text-inverted-subtle' }],
+  ['text-subtle', { color: 'var(--w-text-color-subtle)' }, { autocomplete: 'text-subtle' }],
+]
+
+export const bgColors = [
+  ['bg', { color: 'var(--w-background-color)' }, { autocomplete: 'bg' }],
+  ['bg-subtle', { color: 'var(--w-background-color-subtle)' }, { autocomplete: 'bg-subtle' }],
+]
