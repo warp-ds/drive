2adc25786b4b2d9a8eca8603ffd388dc887041da
Anna Rybkina <anna.rybkina@anicode.se>
2023-02-17 09:17:41 +0100

    Tests for display

---
+++ test/display.js
@@ -0,0 +1,34 @@
+import { setup } from "./_helpers.js";
+import { describe, expect, test } from "vitest";
+import * as displayRules from '../src/_rules/display.js';
+
+
+setup();
+
+describe('display', () => {
+  test('should render styles for static classes', async ({ uno }) => {
+    const staticClasses = Object.values(displayRules).flat().filter(rule => typeof rule[0] === 'string').map(rule => rule[0]);
+
+    const { css } = await uno.generate(staticClasses);
+    expect(css).toMatchSnapshot();
+  });
+  test('should render styles for arbitrary values', async ({ uno }) => {
+    const arbitraryClasses = ['display-unset', 'display-inherit', 'display-revert', 'display-[revert]'];
+
+    const { css } = await uno.generate(arbitraryClasses);
+    expect(css).toMatchInlineSnapshot(`
+      "/* layer: default */
+      .display-\\\\[revert\\\\],
+      .display-revert{display:revert;}
+      .display-inherit{display:inherit;}
+      .display-unset{display:unset;}"
+    `);
+  });
+  test('should not render styles for invalid classes', async ({ uno }) => {
+    const arbitraryClasses = ['unset'];
+
+    const { css } = await uno.generate(arbitraryClasses);
+    expect(css).toMatchInlineSnapshot('""');
+  });
+});
+
