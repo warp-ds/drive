738e0719e9f67b3a1406965d8048d42cb8997026
Dave Honneffer <pearofducks@gmail.com>
2023-02-17 12:00:31 +0100

    Refactor

--- src/postprocessor.js
+++ src/postprocessor.js
@@ -2,13 +2,14 @@ import { escapeSelector } from '@unocss/core'
 
 const prefix = 'w-'
 const classify = (str) => `.${escapeSelector(str)}`
-const coreClasses = [].map(classify) // temporary fixture
 
-export const postprocess = (externalizeCore) => (obj) => {
-  if (externalizeCore && coreClasses.includes(obj.selector)) return obj.entries = null
-  obj.entries.forEach(e => {
-    e[0] = e[0].replace(/^--un-/, `--${prefix}`);
-    if (typeof e[1] === "string") e[1] = e[1].replace(/var\(--un-/g, `var(--${prefix}`);
-  })
+export const postprocess = (externalizeCore, _externalClasses = []) => {
+  const externalClasses = _externalClasses.map(classify)
+  return (obj) => {
+    if (externalizeCore && externalClasses.includes(obj.selector)) return obj.entries = null
+    obj.entries.forEach(e => {
+      e[0] = e[0].replace(/^--un-/, `--${prefix}`);
+      if (typeof e[1] === "string") e[1] = e[1].replace(/var\(--un-/g, `var(--${prefix}`);
+    })
+  }
 }
-
