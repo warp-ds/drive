dc3be90ec29516004b264122f9f281d69dde63be
BalbinaK <balbuhhha@gmail.com>
2023-02-20 13:58:20 +0100

    Use rem handler

--- src/_rules/size.js
+++ src/_rules/size.js
@@ -39,5 +39,5 @@ export const sizes = [
       '(min|max)-w-screen-$breakpoints',
     ],
   }],
-  [/^(min-|max-)?([wh])-\[(\d+)]$/, ([, minmax, wOrH, d]) => ({ [getPropName(minmax, wOrH)]: `${h.number(d) / 10}rem` })],
+  [/^(min-|max-)?([wh])-\[(\d+)]$/, ([, minmax, wOrH, d]) => ({ [getPropName(minmax, wOrH)]: h.rem(d) })],
 ];
