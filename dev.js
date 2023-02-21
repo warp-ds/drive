e73015d4f4de0098ef53d53469342cc518cf1dd6
Anna Rybkina <anna.rybkina@anicode.se>
2023-02-21 11:11:03 +0100

    Add support for providing options from terminal Co-authored-by: BalbinaK <balbuhhha@gmail.com>

--- dev.js
+++ dev.js
@@ -1,9 +1,20 @@
 import { createGenerator } from '@unocss/core';
 import { presetWarp } from '#plugin';
 
-const uno = createGenerator({ presets: [presetWarp()] });
+const options = {};
+const cliClasses = [];
+
+process.argv.slice(2).map(t => {
+  if (t.startsWith('--')) {
+    const option = t.replace('--', '');
+    options[option] = true;
+  } else {
+    cliClasses.push(t);
+  }
+});
+
+const uno = createGenerator({ presets: [presetWarp(options)] });
 const devClasses = ['m-16!', 'opacity-50'];
-const cliClasses = process.argv.slice(2);
 const classes = cliClasses.length ? cliClasses : devClasses;
 const result = await uno.generate(classes);
 console.log(result.css);
