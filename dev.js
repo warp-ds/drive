f5ef8e8db371fee8c48c3fb68cebb744a0118a4a
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-02-22 08:44:03 +0100

    Add support for providing options from terminal (#48)

--- dev.js
+++ dev.js
@@ -1,9 +1,36 @@
+import { parseArgs } from 'node:util';
 import { createGenerator } from '@unocss/core';
 import { presetWarp } from '#plugin';
 
-const uno = createGenerator({ presets: [presetWarp()] });
+
+const {
+  values: { cliClasses, ...options },
+} = parseArgs({
+  options: {
+    cliClasses: {
+      type: 'string',
+      short: 'c',
+    },
+    development: {
+      type: 'boolean',
+    },
+    externalClasses: {
+      type: 'boolean',
+    },
+    externalizeClasses: {
+      type: 'boolean',
+    },
+    usePixels: {
+      type: 'boolean',
+    },
+    usePreflight: {
+      type: 'boolean',
+    },
+  },
+});
+
+const uno = createGenerator({ presets: [presetWarp( options )] });
 const devClasses = ['m-16!', 'opacity-50'];
-const cliClasses = process.argv.slice(2);
-const classes = cliClasses.length ? cliClasses : devClasses;
+const classes = cliClasses ?? devClasses;
 const result = await uno.generate(classes);
 console.log(result.css);
