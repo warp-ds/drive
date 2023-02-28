4b7b4589470434856fd68c93c73ec744220ed839
Anna Rybkina <anna.rybkina@anicode.se>
2023-02-28 11:50:26 +0100

    Missing tests for object-position

--- test/static.js
+++ test/static.js
@@ -1,5 +1,6 @@
 import { setup } from './_helpers.js';
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
+import { positionMap } from '#utils';
 import * as staticRules from '../src/_rules/static.js';
 // TODO: change this to a subpath import when this lands
 // https://github.com/vitejs/vite/pull/7770
@@ -10,4 +11,17 @@ test('static rules do static things', async ({ uno }) => {
   const staticClasses = Object.values(staticRules).flat().filter(rule => typeof rule[0] === 'string').map(rule => rule[0]);
   const { css } = await uno.generate(staticClasses);
   expect(css).toMatchSnapshot();
+});
+
+describe('static rules for object position', () => {
+  test('supports all predefined values in the position map', async ({ uno }) => {
+    const staticClasses = Object.keys(positionMap).map(position => `object-${position}`);
+    const { css } = await uno.generate(staticClasses);
+    expect(css).toMatchSnapshot();
+  });
+  test('do not support invalid values', async ({ uno }) => {
+    const staticClasses = ['object-position', 'object-right-left'];
+    const { css } = await uno.generate(staticClasses);
+    expect(css).toMatchInlineSnapshot('""');
+  });
 });
