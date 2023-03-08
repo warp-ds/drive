888220e9a2f96d8261952eee7cacd799086988c7
Dave Honneffer <pearofducks@gmail.com>
2023-03-08 11:58:46 +0100

    Bump deps, add test for Uno's HTML parser

--- test/general.js
+++ test/general.js
@@ -30,6 +30,17 @@ test(`autocomplete doesn't throw`, async ({ uno }) => {
   });
 });
 
+test(`the HTML parser is sane`, async ({ uno }) => {
+  const { css } = await uno.generate(`<div class="before:content-['before-stuff'] sm:grid"></div>`);
+  expect(css).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .before\\\\:content-\\\\[\\\\'before-stuff\\\\'\\\\]::before{content:'before-stuff';}
+    @media (min-width: 480px){
+    .sm\\\\:grid{display:grid;}
+    }"
+  `);
+});
+
 test('can generate pixel values for theme', async () => {
   const uno = getGenerator({ usePixels: true });
   const { css } = await uno.generate(['pt-8', 'bottom-4', '-ml-32']);
