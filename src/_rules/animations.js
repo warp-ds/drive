96f2951c622154ee9f586c2825ccdbf7ba26efd3
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-03-28 14:11:58 +0200

    chore: add bg size to animation (#80)

--- src/_rules/animations.js
+++ src/_rules/animations.js
@@ -1,7 +1,7 @@
 import { escapeSelector } from '@unocss/core';
 
 // Very hardcoded styling for loading, not decided if it will be themable so let's have this for now and change if applicable later
-const inProgress = 'background-image:linear-gradient(135deg,rgba(0, 0, 0, 0.05) 25%,transparent 0,transparent 50%,rgba(0, 0, 0, 0.05) 0,rgba(0, 0, 0, 0.05) 75%,transparent 0,transparent) !important;animation: animate-inprogress 3s linear infinite;';
+const inProgress = 'background-image:linear-gradient(135deg,rgba(0, 0, 0, 0.05) 25%,transparent 0,transparent 50%,rgba(0, 0, 0, 0.05) 0,rgba(0, 0, 0, 0.05) 75%,transparent 0,transparent) !important;background-size: 30px 30px;animation: animate-inprogress 3s linear infinite;';
 const keyFrames = '@keyframes animate-inprogress {0% {background-position: 0 0;}to {background-position: 60px 0;}}';
 
 export const animations = [
