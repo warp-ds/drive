05b0c54884b2b0905f666ed033903fe9886ed2bf
Anna Rybkina <anna.rybkina@anicode.se>
2023-04-18 15:18:32 +0200

    fix: add rules and tests for caret

--- src/_rules/color.js
+++ src/_rules/color.js
@@ -2,16 +2,23 @@ export const opacity = [
   [/^opacity-(\d+)$/, ([, d], { theme }) => ({ opacity: theme.opacity[d] }), { autocomplete: 'opacity-${opacity}' }],
 ];
 
+export const caretColors = [
+  ['caret', { 'caret-color': 'var(--w-color-input-caret)' }],
+  ['caret-inherit', { 'caret-color': 'inherit'  }],
+  ['caret-current', { 'caret-color': 'currentColor' }],
+  ['caret-transparent', { 'caret-color': 'transparent' }],
+];
+
 export const textColors = [
-  ['text', { color: 'var(--w-text-color)' }],
-  ['text-inverted', { color: 'var(--w-text-color-inverted)' }],
-  ['text-inverted-subtle', { color: 'var(--w-text-color-inverted-subtle)' }],
-  ['text-subtle', { color: 'var(--w-text-color-subtle)' }],
+  ['text', { color: 'var(--w-color-text)' }],
+  ['text-inverted', { color: 'var(--w-color-text-inverted)' }],
+  ['text-inverted-subtle', { color: 'var(--w-color-text-inverted-subtle)' }],
+  ['text-subtle', { color: 'var(--w-color-text-subtle)' }],
 ];
 
 export const bgColors = [
-  ['bg', { 'background-color': 'var(--w-background-color)' }],
-  ['bg-subtle', { 'background-color': 'var(--w-background-color-subtle)' }],
+  ['bg', { 'background-color': 'var(--w-color-background)' }],
+  ['bg-subtle', { 'background-color': 'var(--w-color-background-subtle)' }],
   ['bg-inherit', { 'background-color': 'inherit' }],
   ['bg-transparent', { 'background-color': 'transparent' }],
   ['bg-current', { 'background-color': 'currentColor' }],
