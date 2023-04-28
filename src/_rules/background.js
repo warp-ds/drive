d908e249848944a0bdd42c2b598a1c21074cb817
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-04-28 10:34:45 +0200

    fix: extend regex for the background arbitrary values (#92)

--- src/_rules/background.js
+++ src/_rules/background.js
@@ -38,5 +38,5 @@ export const backgrounds = [
   ...makeGlobalStaticRules('bg-origin', 'background-origin'),
 
   //arbitrary
-  [/^bg-\[url\('(.+)'\)]/, ([, p]) => ({ 'background-image': `url('${p}')` })],
+  [/^bg-\[url\(['|"](.+)['|"]\)]/, ([, p]) => ({ 'background-image': `url("${p}")` })],
 ];
