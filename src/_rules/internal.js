c2b1e25fa311564e083969b465c0dc9c9ce8403d
BalbinaK <balbuhhha@gmail.com>
2023-05-03 14:00:44 +0200

    chore: rename i-elevation prefix to i-shadow

--- src/_rules/internal.js
+++ src/_rules/internal.js
@@ -4,7 +4,7 @@ export const internalRules = [
   [/^i-bg-(.+)$/, ([, cssvar]) => ({ 'background-color': h.warpToken(cssvar) })],
   [/^i-text-(.+)$/, ([, cssvar]) => ({ color: h.warpToken(cssvar) })],
   [/^i-border-(.+)$/, ([, cssvar]) => ({ 'border-color': h.warpToken(cssvar) })],
-  [/^i-elevation-(.+)$/, ([, cssvar]) => ({ 'box-shadow': h.warpToken(cssvar) })],
+  [/^i-shadow-(.+)$/, ([, cssvar]) => ({ 'box-shadow': h.warpToken(cssvar) })],
   [/^i-border-([rltb])-(.+)$/, ([, direction, cssvar]) => {
     if (direction in directionMap && cssvar != null) {
       return directionMap[direction].map(
--- test/internal.js
+++ test/internal.js
@@ -12,9 +12,9 @@ test('it generates css based on warp tokens', async ({ uno }) => {
     'i-border-r-$color-alert-info-border',
     'i-border-t-$color-alert-info-border',
     'i-border-b-$color-alert-info-border',
-    'i-elevation-$shadow-card',
-    'i-elevation-$shadow-buttongroup',
-    'i-elevation-$shadow-tooltip',
+    'i-shadow-$shadow-card',
+    'i-shadow-$shadow-buttongroup',
+    'i-shadow-$shadow-tooltip',
   ];
   const anticlasses = [
     'i-bg-foos-bars',
@@ -27,7 +27,7 @@ test('it generates css based on warp tokens', async ({ uno }) => {
     'i-border-bl-$color-alert-info-border',
     'i-border-tbl-$color-alert-info-border',
     'i-$shadow-card',
-    'i-shadow-$shadow-card',
+    'i-elevation-$shadow-card',
   ];
   const { css } = await uno.generate([...classes, ...anticlasses]);
   expect(css).toMatchSnapshot();
