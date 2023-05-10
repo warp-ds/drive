54a300285161d8e9c0a101eff4132d2439abda7b
Caroline Gard <caroline.gard@mpyadigital.com>
2023-05-10 15:44:31 +0200

    feat(touch-actions): add support for touch actions (#97)

--- src/_rules/static.js
+++ src/_rules/static.js
@@ -174,3 +174,16 @@ export const mixBlendModes = [
   ['mix-blend-normal', { 'mix-blend-mode': 'normal' }],
   ...makeGlobalStaticRules('mix-blend'),
 ];
+
+export const touchAction = [
+  ['touch-auto', { 'touch-action': 'auto' }],
+  ['touch-none', { 'touch-action': 'none' }],
+  ['touch-pan-x', { 'touch-action': 'pan-x' }],
+  ['touch-pan-y', { 'touch-action': 'pan-y' }],
+  ['touch-pan-left', { 'touch-action': 'pan-left' }],
+  ['touch-pan-right', { 'touch-action': 'pan-right' }],
+  ['touch-pan-up', { 'touch-action': 'pan-up' }],
+  ['touch-pan-down', { 'touch-action': 'pan-down' }],
+  ['touch-pinch-zoom', { 'touch-action': 'pinch-zoom' }],
+  ['touch-manipulation', { 'touch-action': 'manipulation' }],
+];
