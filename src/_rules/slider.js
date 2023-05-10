9f2927e0ffdf7d8d5d6c4f3446f37694d69b5368
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-10 11:11:37 +0200

    feat(slider): add border shadow as hardcoded classnames
    This to make the hover and active states to work for now. Currently the same colors for all themes

---
+++ src/_rules/slider.js
@@ -0,0 +1,15 @@
+// Hardcoded styling for slider border shadow, will be modified and handled with correct color variables at a later stage
+const defaultColor = 'rgba(0, 0, 0, .08)';
+const sliderStates = {
+  active: '0 0 0 8px',
+  hover: '0 0 0 6px',
+};
+
+export const sliderHandleShadow = [
+  [
+    /^slider-handle-shadow-(active|hover)$/,
+    ([, state]) => ({
+      'box-shadow': `${sliderStates[state]} ${defaultColor})`,
+    }),
+  ],
+];
