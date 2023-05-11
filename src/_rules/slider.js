4b2af46c0586477e2e3d93b36c835837ede816bc
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-11 09:26:05 +0200

    feat(slider): add css for slider

--- src/_rules/slider.js
+++ src/_rules/slider.js
@@ -1,3 +1,5 @@
+import { entriesToCss } from '@unocss/core';
+
 // Hardcoded styling for slider border shadow, will be modified and handled with correct color variables at a later stage
 // TODO: Make the colors for the slider shadow themeable
 const defaultColor = 'rgba(0, 0, 0, .08)';
@@ -12,5 +14,17 @@ export const sliderHandleShadow = [
     ([, state]) => ({
       'box-shadow': `${sliderStates[state]} ${defaultColor}`,
     }),
+  ],
+];
+
+export const sliderOutlineStyles = entriesToCss(Object.entries({
+  outline: '2px solid rgba(0, 0, 0, 0)',
+  'outline-offset': '2px',
+}));
+
+export const sliderOutlineNone = [
+  [
+    /^slider-outline-none$/,
+    () => (sliderOutlineStyles),
   ],
 ];
