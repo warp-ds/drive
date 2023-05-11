fa4f8cae539ef93f94ff2530117103c3c68443f8
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-11 09:26:47 +0200

    test(slider): add test for slider

--- test/slider.js
+++ test/slider.js
@@ -4,13 +4,19 @@ import { describe, expect, test } from 'vitest';
 setup();
 
 describe('slider', () => {
-  test('get active slider shadow ', async ({ uno }) => {
+  test('get active slider shadow', async ({ uno }) => {
     const { css } = await uno.generate(['slider-handle-shadow-active']);
     expect(css).toMatchSnapshot();
   });
 
-  test('get hover slider shadow ', async ({ uno }) => {
+  test('get hover slider shadow', async ({ uno }) => {
     const { css } = await uno.generate(['slider-handle-shadow-hover']);
+    expect(css).toMatchSnapshot();
+  });
+
+  test('get styling for slider outline none', async ({ uno }) => {
+    const { css } = await uno.generate(['slider-outline-none']);
+    console.log("css", css);
     expect(css).toMatchSnapshot();
   });
 });
