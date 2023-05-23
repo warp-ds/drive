f53afd6e9213838825baace549822cc22fc5c0f0
Anna Rybkina <anna.rybkina@anicode.se>
2023-05-23 10:55:33 +0200

    fix: remove unnecessary stuff

--- src/_preflights/index.js
+++ src/_preflights/index.js
@@ -1,6 +1,5 @@
 import { twReset } from './tw-reset.js';
-import { formPreflight } from './forms/export.js';
 import { transformBase } from './transform.js';
 import { typographyBase } from './typography.js';
 
-export const preflights = [twReset, transformBase, formPreflight, typographyBase];
+export const preflights = [twReset, transformBase, typographyBase];
