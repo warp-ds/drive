51c976ab77ded20ff21b7ae84946c206a1e22d54
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-23 09:13:33 +0200

    fix(shadows): remove blank line

--- src/_rules/shadow.js
+++ src/_rules/shadow.js
@@ -11,7 +11,6 @@ export const shadows = [
   [/^shadow-(small|medium|large|xlarge)$/, ([, size]) => ({ 'box-shadow': `var(--w-shadow-${size}, ${boxShadowDefaults[size]})` })],
 ];
 
-
 export const dropShadow = [
   [/^drop-shadow$/, () => ({ 'filter': 'drop-shadow(rgba(64, 64, 64, 0.24) 0px 3px 8px) drop-shadow(rgba(64, 64, 64, 0.16) 0px 3px 6px)' })],
 ];
