88a20aa8c609158482fa963c1b5550903746ef15
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-23 12:26:30 +0200

    fix(drop-shadow): rename to s, m, l, xl

--- src/_rules/shadow.js
+++ src/_rules/shadow.js
@@ -1,4 +1,4 @@
-// Todo: Handle dynamic rgba colors instead of constant colors
+// TODO: Handle dynamic rgba colors instead of constant colors
 
 const defaultColor = 'rgba(0, 0, 0, .2)';
 
@@ -9,6 +9,7 @@ const boxShadowDefaults = {
   xlarge: `0 9px 12px ${defaultColor}, 0 14px 28px ${defaultColor}`,
 };
 
+// TODO: rename to -s, -m, -l, -xs
 export const shadows = [
   [/^shadow-(small|medium|large|xlarge)$/, ([, size]) => ({ 'box-shadow': `var(--w-shadow-${size}, ${boxShadowDefaults[size]})` })],
 ];
@@ -16,12 +17,12 @@ export const shadows = [
 const dropShadowDefaultColor = '64, 64, 64';
 
 const dropShadowDefaults = {
-  small: [`drop-shadow(rgba(${dropShadowDefaultColor}, .16) 0 1px 6px) drop-shadow(rgba(${dropShadowDefaultColor}, .24) 0 1px 1px)`],
-  medium: [`drop-shadow(rgba(${dropShadowDefaultColor}, .24) 0 3px 8px) drop-shadow(rgba(${dropShadowDefaultColor}, .16) 0 3px 6px)`],
-  large: [`drop-shadow(rgba(${dropShadowDefaultColor}, .23) 0 6px 8px) drop-shadow(rgba(${dropShadowDefaultColor}, .19) 0 10px 20px)`],
-  xlarge: [`drop-shadow(rgba(${dropShadowDefaultColor}, .22) 0 9px 12px) drop-shadow(rgba(${dropShadowDefaultColor}, .25) 0 14px 28px)`],
+  s: [`drop-shadow(rgba(${dropShadowDefaultColor}, .16) 0 1px 6px) drop-shadow(rgba(${dropShadowDefaultColor}, .24) 0 1px 1px)`],
+  m: [`drop-shadow(rgba(${dropShadowDefaultColor}, .24) 0 3px 8px) drop-shadow(rgba(${dropShadowDefaultColor}, .16) 0 3px 6px)`],
+  l: [`drop-shadow(rgba(${dropShadowDefaultColor}, .23) 0 6px 8px) drop-shadow(rgba(${dropShadowDefaultColor}, .19) 0 10px 20px)`],
+  xl: [`drop-shadow(rgba(${dropShadowDefaultColor}, .22) 0 9px 12px) drop-shadow(rgba(${dropShadowDefaultColor}, .25) 0 14px 28px)`],
 };
 
 export const dropShadows = [
-  [/^drop-shadow-(small|medium|large|xlarge)$/, ([, size]) => ({ 'filter': dropShadowDefaults[size] })],
+  [/^drop-shadow-(s|m|l|xl)$/, ([, size]) => ({ 'filter': dropShadowDefaults[size] })],
 ];
