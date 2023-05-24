9b68cbe52739127773edbf938929e1b21b17d8e6
Caroline Gard <caroline.gard@mpyadigital.com>
2023-05-24 17:01:59 +0200

    feat(dropshadow): add drop shadow (#105)

--- test/shadow.js
+++ test/shadow.js
@@ -8,3 +8,9 @@ test('shadows work', async ({ uno }) => {
   const { css } = await uno.generate(classes);
   expect(css).toMatchSnapshot();
 });
+
+test('drop shadows', async ({ uno }) => {
+  const classes = ['drop-shadow-s', 'drop-shadow-m', 'drop-shadow-l', 'drop-shadow-xl'];
+  const { css } = await uno.generate(classes);
+  expect(css).toMatchSnapshot();
+});
