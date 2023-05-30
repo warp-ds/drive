274a6f7cd8f21f21d77baa83171d63698669b83e
Anna Rybkina <anna.rybkina@anicode.se>
2023-05-30 16:09:56 +0200

    fix: add minified resets version instead

--- src/_preflights/resets.js
+++ src/_preflights/resets.js
@@ -1,4 +1,4 @@
 export const resets = {
   layer: 'preflights',
-  getCSS: async () => (await fetch('https://assets.finn.no/pkg/@warp-ds/css/v1/resets.css')).text(),
+  getCSS: async () => (await fetch('https://assets.finn.no/pkg/@warp-ds/css/v1/resets.min.css')).text(),
 };
