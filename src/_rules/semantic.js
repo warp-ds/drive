4036aa1c431d90638d663e2a23eb2de210e8f892
Magnus Hübner <magnus.hubner@aptitud.se>
2023-06-13 15:59:27 +0200

    fix: Added rules for directional semantic border classes

--- src/_rules/semantic.js
+++ src/_rules/semantic.js
@@ -1,7 +1,10 @@
-import { handler as h } from '#utils';
+import { directionMap, handler as h } from '#utils';
 
 export const semanticRules = [
   [/^s-bg-(.+)$/, ([, cssvar]) => ({ 'background-color': h.semanticToken(`background-${cssvar}`) })],
   [/^s-text-(.+)$/, ([, cssvar]) => ({ color: h.semanticToken(`text-${cssvar}`) })],
   [/^s-border-(.+)$/, ([, cssvar]) => ({ 'border-color': h.semanticToken(`border-${cssvar}`) })],
+  [/^s-border-([lrtbxy])-(.+)$/, ([, direction, cssvar]) => directionMap[direction]?.map(
+    (dir) => [`border${dir}-color`, h.semanticToken(`border-${cssvar}`)],
+  )],
 ];
