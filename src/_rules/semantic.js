afc0354b777c76bed0f962b3fe1099303488353b
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2023-06-15 11:51:44 +0200

    feat(semantic.js): add rules for semantic icon colors (#124)

--- src/_rules/semantic.js
+++ src/_rules/semantic.js
@@ -3,6 +3,7 @@ import { directionMap, handler as h } from '#utils';
 export const semanticRules = [
   [/^s-bg-(.+)$/, ([, cssvar]) => ({ 'background-color': h.semanticToken(`background-${cssvar}`) })],
   [/^s-text-(.+)$/, ([, cssvar]) => ({ color: h.semanticToken(`text-${cssvar}`) })],
+  [/^s-icon-(.+)$/, ([, cssvar]) => ({ color: h.semanticToken(`icon-${cssvar}`) })],
   [/^s-border-(.+)$/, ([, cssvar]) => ({ 'border-color': h.semanticToken(`border-${cssvar}`) })],
   [/^s-border-([lrtbxy])-(.+)$/, ([, direction, cssvar]) => directionMap[direction]?.map(
     (dir) => [`border${dir}-color`, h.semanticToken(`border-${cssvar}`)],
