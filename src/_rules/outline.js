e919c5f3e85bbc7f695844b6ea5352b675625f85
AnnaRybkina <anna.rybkina@schibsted.com>
2023-06-16 09:04:25 +0200

    fix: improvements after code review comments

--- src/_rules/outline.js
+++ src/_rules/outline.js
@@ -6,15 +6,15 @@ const outlineNone = {
 };
 
 export const outlineStyle = [
-  ["outline-none", { ... outlineNone }],
+  ["outline-none", { ...outlineNone }],
   ["outline", { "outline-style": "solid" }],
   ["outline-dashed", { "outline-style": "dashed" }],
   ["outline-dotted", { "outline-style": "dotted" }],
   ["outline-double", { "outline-style": "double" }],
 ];
 export const outlineWidth = [
-  [/^outline-(\d+)$/, ([, w]) => ({ ['outline-width']: lineWidth?.[w ?? 1] }), { autocomplete: `(outline)-(${Object.keys(lineWidth).join('|')})` }],
+  [/^outline-(\d+)$/, ([, w]) => ({ ['outline-width']: lineWidth?.[w ?? 1] }), { autocomplete: `outline-(${Object.keys(lineWidth).join('|')})` }],
 ];
 export const outlineOffset = [
-  [/^outline-offset-(\d+)$/,  ([, w]) => ({ [`outline-offset`]: lineWidth?.[w ?? 1] }), { autocomplete: `(outline-offset)-(${Object.keys(lineWidth).join('|')})` }],
+  [/^outline-offset-(\d+)$/,  ([, w]) => ({ [`outline-offset`]: lineWidth?.[w ?? 1] }), { autocomplete: `outline-offset-(${Object.keys(lineWidth).join('|')})` }],
 ];
