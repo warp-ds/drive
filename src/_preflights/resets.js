7d363c78c7fc3045df4435e516888d2d907da5c3
CarolineGard <caroline.gard@mpyadigital.com>
2023-06-22 15:20:59 +0200

    fix(reset): update fetch api to be compatible with older node versions such as node v.16

--- src/_preflights/resets.js
+++ src/_preflights/resets.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export const resets = {
   layer: 'preflights',
   getCSS: async () => (await fetch('https://assets.finn.no/pkg/@warp-ds/css/v1/resets.min.css')).text(),
