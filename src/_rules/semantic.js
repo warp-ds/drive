805526df70a339747a45ace1184b060dc4483ce1
Magnus Hübner <magnuh@users.noreply.github.com>
2023-06-28 13:42:59 +0200

    fix: Updated rules for semantic colors to handle base level token names without ending "default" suffix (#133)

--- src/_rules/semantic.js
+++ src/_rules/semantic.js
@@ -1,10 +1,17 @@
 import { directionMap, handler as h } from '#utils';
 
 export const semanticRules = [
+  [/^s-bg$/, () => ({ 'background-color': h.semanticToken('background') })],
   [/^s-bg-(.+)$/, ([, cssvar]) => ({ 'background-color': h.semanticToken(`background-${cssvar}`) })],
+  [/^s-text$/, () => ({ color: h.semanticToken('text') })],
   [/^s-text-(.+)$/, ([, cssvar]) => ({ color: h.semanticToken(`text-${cssvar}`) })],
+  [/^s-icon$/, () => ({ color: h.semanticToken('icon') })],
   [/^s-icon-(.+)$/, ([, cssvar]) => ({ color: h.semanticToken(`icon-${cssvar}`) })],
+  [/^s-border$/, ([, cssvar]) => ({ 'border-color': h.semanticToken('border') })],
   [/^s-border-(.+)$/, ([, cssvar]) => ({ 'border-color': h.semanticToken(`border-${cssvar}`) })],
+  [/^s-border-([lrtbxy])$/, ([, direction]) => directionMap[direction]?.map(
+    (dir) => [`border${dir}-color`, h.semanticToken('border')],
+  )],
   [/^s-border-([lrtbxy])-(.+)$/, ([, direction, cssvar]) => directionMap[direction]?.map(
     (dir) => [`border${dir}-color`, h.semanticToken(`border-${cssvar}`)],
   )],
