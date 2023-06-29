7b5428e4a75627d8c7c5465b93ce96eb3a5d4555
Dave Honneffer <pearofducks@gmail.com>
2023-06-29 08:58:24 +0200

    fix: cache the preflight instead of requesting every rebuild

--- src/_preflights/resets.js
+++ src/_preflights/resets.js
@@ -1,4 +1,6 @@
+const reset = (await fetch('https://assets.finn.no/pkg/@warp-ds/css/v1/resets.min.css')).text();
+
 export const resets = {
   layer: 'preflights',
-  getCSS: async () => (await fetch('https://assets.finn.no/pkg/@warp-ds/css/v1/resets.min.css')).text(),
+  getCSS: () => reset,
 };
