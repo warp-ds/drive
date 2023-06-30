afadb496f819a7965eb688429a512548cfa1ad7c
Magnus Hübner <magnus.hubner@aptitud.se>
2023-06-30 15:44:26 +0200

    fix: Remove old fallback values from box shadows

--- src/_rules/shadow.js
+++ src/_rules/shadow.js
@@ -1,16 +1,5 @@
-// TODO: Handle dynamic rgba colors instead of constant colors
-
-const defaultColor = 'rgba(0, 0, 0, .2)';
-
-const boxShadowDefaults = {
-  s: `0 1px 6px ${defaultColor}, 0 1px 1px ${defaultColor}`,
-  m: `0 3px 8px ${defaultColor}, 0 3px 6px ${defaultColor}`,
-  l: `0 6px 8px ${defaultColor}, 0 10px 20px ${defaultColor}`,
-  xl: `0 9px 12px ${defaultColor}, 0 14px 28px ${defaultColor}`,
-};
-
 export const shadows = [
-  [/^shadow-(s|m|l|xl)$/, ([, size]) => ({ 'box-shadow': `var(--w-shadow-${size}, ${boxShadowDefaults[size]})` })],
+  [/^shadow-(s|m|l|xl)$/, ([, size]) => ({ 'box-shadow': `var(--w-shadow-${size})` })],
 ];
 
 const dropShadowDefaultColor = '64, 64, 64';
