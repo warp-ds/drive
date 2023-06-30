948c5173855efb3a68da9abad08752f50e24dc1d
AnnaRybkina <anna.rybkina@schibsted.com>
2023-06-30 08:52:28 +0200

    fix: check for type string before matching the class in the last-child transformation

--- src/_variants/lastChild.js
+++ src/_variants/lastChild.js
@@ -1,4 +1,5 @@
 export const variantLastChild = (matcher) => {
+  if (typeof matcher !== 'string') return;
   if (!matcher?.startsWith('last-child:')) { return matcher; };
   return {
     matcher: matcher.slice(11),
