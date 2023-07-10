43fbd0e60d5f29c223b7bb870dad2483b93f8577
Silje Christensen <silje.christensen@cognite.com>
2023-07-10 08:21:58 +0200

    fix: change from border-white to border-inverted

--- src/_rules/border.js
+++ src/_rules/border.js
@@ -18,7 +18,7 @@ const borderStyles = [
 export const borders = [
   [/^border$/, handlerBorder],
   [/^border-transparent$/, () => ({ 'border-color': 'transparent' })],
-  [/^border-white$/, () => ({ 'border-color': 'var(--w-white)' })],
+  [/^border-inverted$/, () => ({ 'border-color': 'var(--w-s-border-inverted)' })],
   [/^border-inherit$/, () => ({ 'border-color': 'inherit' })],
   [/^border-current$/, () => ({ 'border-color': 'currentColor' })],
   [/^border()-(\d+)$/, handlerBorder, { autocomplete: "(border)-<directions>" }],
