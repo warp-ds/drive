aeb47118594a896bde39c2b8200863854bc4e1cb
Silje Christensen <silje.christensen@cognite.com>
2023-07-13 13:07:41 +0200

    fix: avoid null issue and use empty array instead

--- src/postprocessor.js
+++ src/postprocessor.js
@@ -6,7 +6,7 @@ const classify = (str) => `.${escapeSelector(str)}`;
 export const postprocess = (shouldExternalize, _externalClasses = []) => {
   const externalClasses = _externalClasses.map(classify);
   return (obj) => {
-    if (shouldExternalize && externalClasses.includes(obj.selector)) return obj.entries = null;
+    if (shouldExternalize && externalClasses.includes(obj.selector)) return obj.entries = [];
     obj.entries.forEach(e => {
       e[0] = e[0].replace(/^--un-/, `--${prefix}`);
       if (typeof e[1] === "string") e[1] = e[1].replace(/var\(--un-/g, `var(--${prefix}`);
