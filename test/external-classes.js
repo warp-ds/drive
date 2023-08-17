1c2f8d132dfa6f094992c72890fe9374ad464cd1
BalbinaK <balbuhhha@gmail.com>
2023-08-17 16:10:25 +0200

    test: add tests for external classes

---
+++ test/external-classes.js
@@ -0,0 +1,22 @@
+import { setup } from './_helpers.js';
+import { describe, expect, test } from 'vitest';
+
+setup({ externalizeClasses: true, externalClasses: ["mt-32", "mb-32"] });
+
+describe('external classes', () => {
+  test('externaliized classes of mt-32 and mb-32 should be excluded from the css', async ({ uno }) => {
+    const classes = ['mt-16', 'mt-32', 'mb-32'];
+    const { css } = await uno.generate(classes);
+    expect(css).toMatchSnapshot();
+  });
+  test('externaliized class of mt-32 should be excluded from the css', async ({ uno }) => {
+    const classes = ['mt-16', 'mt-32', 'mb-24'];
+    const { css } = await uno.generate(classes);
+    expect(css).toMatchSnapshot();
+  });
+  test('externaliized class of mt-32 should be included if used with modifier', async ({ uno }) => {
+    const classes = ['sm:mt-32', 'md:mt-32', 'active:mt-32'];
+    const { css } = await uno.generate(classes);
+    expect(css).toMatchSnapshot();
+  });
+});
