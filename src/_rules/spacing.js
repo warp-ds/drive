fa8cf3aa121dbd0da0657772c4732a10472662bd
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2023-12-07 13:36:37 +0100

    fix: add support for arbitrary values for padding + margin

--- src/_rules/spacing.js
+++ src/_rules/spacing.js
@@ -1,15 +1,29 @@
-import { directionSize } from '#utils';
+import { directionSize, resolveArbitraryValues, directionMap } from '#utils';
 
 // negatives come in via the negative variant
 export const padding = [
   // empty capture group here sets an empty string for 'direction' instead of undefined
-  [/^p()-(.+)$/, directionSize('padding'), { autocomplete: '(m|p)-<num>' }],
-  [/^p([xy])-(.+)$/, directionSize('padding')],
-  [/^p([rltb])-(.+)$/, directionSize('padding'), { autocomplete: '(m|p)<directions>-<num>' }],
+  [/^p()-(.\d*)$/, directionSize('padding'), { autocomplete: '(m|p)-<num>' }],
+  [/^p([xy])-(.\d*)$/, directionSize('padding')],
+  [/^p([rltb])-(.\d*)$/, directionSize('padding'), { autocomplete: '(m|p)<directions>-<num>' }],
+  // matching arbitrary values
+  [/^p-\[(.\d*)(rem|px)?]$/, ([, value, unit], context) => ({ padding: resolveArbitraryValues(value, unit, context) })],
+  [/^p([xy])-\[(.\d*)(rem|px)?]$/, ([, direction, value, unit], context) => 
+    directionMap[direction].map(i => [`padding${i}`, resolveArbitraryValues(value, unit, context)])
+  ],
+  [/^p([rltb])-\[(.\d*)(rem|px)?]$/, ([, direction, value, unit], context) => 
+    directionMap[direction].map(i => [`padding${i}`, resolveArbitraryValues(value, unit, context)])
+  ],
 ];
 
 export const margin = [
   [/^m()-(.+)$/, directionSize('margin')],
   [/^m([xy])-(.+)$/, directionSize('margin')],
   [/^m([rltb])-(.+)$/, directionSize('margin')],
+  // matching arbitrary values
+  [/^m-\[(.\d*)(rem|px)?]$/, ([, value, unit], context) => ({ margin: resolveArbitraryValues(value, unit, context) })],
+  [/^m([xy])-\[(.\d*)(rem|px)?]$/, ([, direction, value, unit], context) => 
+  directionMap[direction].map(i => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
+  [/^m([rltb])-\[(.\d*)(rem|px)?]$/, ([, direction, value, unit], context) => 
+  directionMap[direction].map(i => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
 ];
--- test/spacing.js
+++ test/spacing.js
@@ -47,3 +47,74 @@ test('margin skips bad values', async ({ uno }) => {
   const { css } = await uno.generate(classes);
   expect(css).toMatchInlineSnapshot('""');
 });
+
+test('arbitrary padding works', async ({ uno }) => {
+  const classes = ['p-[8]','p-[8rem]','p-[8px]',
+  'px-[8]','px-[8rem]','px-[8px]',
+  'py-[8]','py-[8rem]','py-[8px]',
+  'pr-[8]','pr-[8rem]','pr-[8px]',
+  'pl-[8]','pl-[8rem]','pl-[8px]',
+  'pt-[8]','pt-[8rem]','pt-[8px]',
+  'pb-[8]','pb-[8rem]','pb-[8px]',
+];
+  const { css } = await uno.generate(classes);
+  expect(css).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .p-\\\\[8\\\\]{padding:0.8rem;}
+    .p-\\\\[8px\\\\]{padding:8px;}
+    .p-\\\\[8rem\\\\]{padding:8rem;}
+    .px-\\\\[8\\\\]{padding-left:0.8rem;padding-right:0.8rem;}
+    .px-\\\\[8px\\\\]{padding-left:8px;padding-right:8px;}
+    .px-\\\\[8rem\\\\]{padding-left:8rem;padding-right:8rem;}
+    .py-\\\\[8\\\\]{padding-top:0.8rem;padding-bottom:0.8rem;}
+    .py-\\\\[8px\\\\]{padding-top:8px;padding-bottom:8px;}
+    .py-\\\\[8rem\\\\]{padding-top:8rem;padding-bottom:8rem;}
+    .pb-\\\\[8\\\\]{padding-bottom:0.8rem;}
+    .pb-\\\\[8px\\\\]{padding-bottom:8px;}
+    .pb-\\\\[8rem\\\\]{padding-bottom:8rem;}
+    .pl-\\\\[8\\\\]{padding-left:0.8rem;}
+    .pl-\\\\[8px\\\\]{padding-left:8px;}
+    .pl-\\\\[8rem\\\\]{padding-left:8rem;}
+    .pr-\\\\[8\\\\]{padding-right:0.8rem;}
+    .pr-\\\\[8px\\\\]{padding-right:8px;}
+    .pr-\\\\[8rem\\\\]{padding-right:8rem;}
+    .pt-\\\\[8\\\\]{padding-top:0.8rem;}
+    .pt-\\\\[8px\\\\]{padding-top:8px;}
+    .pt-\\\\[8rem\\\\]{padding-top:8rem;}"
+  `);
+});
+test('arbitrary margin works', async ({ uno }) => {
+  const classes = ['m-[8]','m-[8rem]','m-[8px]',
+  'mx-[8]','mx-[8rem]','mx-[8px]',
+  'my-[8]','my-[8rem]','my-[8px]',
+  'mr-[8]','mr-[8rem]','mr-[8px]',
+  'ml-[8]','ml-[8rem]','ml-[8px]',
+  'mt-[8]','mt-[8rem]','mt-[8px]',
+  'mb-[8]','mb-[8rem]','mb-[8px]',
+];
+  const { css } = await uno.generate(classes);
+  expect(css).toMatchInlineSnapshot(`
+    "/* layer: default */
+    .m-\\\\[8\\\\]{margin:0.8rem;}
+    .m-\\\\[8px\\\\]{margin:8px;}
+    .m-\\\\[8rem\\\\]{margin:8rem;}
+    .mx-\\\\[8\\\\]{margin-left:0.8rem;margin-right:0.8rem;}
+    .mx-\\\\[8px\\\\]{margin-left:8px;margin-right:8px;}
+    .mx-\\\\[8rem\\\\]{margin-left:8rem;margin-right:8rem;}
+    .my-\\\\[8\\\\]{margin-top:0.8rem;margin-bottom:0.8rem;}
+    .my-\\\\[8px\\\\]{margin-top:8px;margin-bottom:8px;}
+    .my-\\\\[8rem\\\\]{margin-top:8rem;margin-bottom:8rem;}
+    .mb-\\\\[8\\\\]{margin-bottom:0.8rem;}
+    .mb-\\\\[8px\\\\]{margin-bottom:8px;}
+    .mb-\\\\[8rem\\\\]{margin-bottom:8rem;}
+    .ml-\\\\[8\\\\]{margin-left:0.8rem;}
+    .ml-\\\\[8px\\\\]{margin-left:8px;}
+    .ml-\\\\[8rem\\\\]{margin-left:8rem;}
+    .mr-\\\\[8\\\\]{margin-right:0.8rem;}
+    .mr-\\\\[8px\\\\]{margin-right:8px;}
+    .mr-\\\\[8rem\\\\]{margin-right:8rem;}
+    .mt-\\\\[8\\\\]{margin-top:0.8rem;}
+    .mt-\\\\[8px\\\\]{margin-top:8px;}
+    .mt-\\\\[8rem\\\\]{margin-top:8rem;}"
+  `);
+});
