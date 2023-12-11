9f14dd8d1060fc62991130145ac058c82693b651
Magnus Hübner <magnus.hubner@aptitud.se>
2023-12-11 18:01:32 +0100

    chore: misc cleanup of border tests

--- test/border.js
+++ test/border.js
@@ -5,50 +5,50 @@ import { lineWidth } from '#theme';
 setup();
 
 describe("border", () => {
-  test("supports x|y with value and without", async (t) => {
+  test("supports x|y with value and without", async ({ uno }) => {
     const x = ["border-x", "border-x-0", "border-x-2", "border-x-4", "border-x-8"];
     const y = ["border-y", "border-y-0", "border-y-2", "border-y-4", "border-y-8"];
 
     const classes = [...x, ...y];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting border width", async (t) => {
+  test("supports setting border width", async ({ uno }) => {
     const classes = ["border", "border-0", "border-2", "border-4", "border-8"];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting arbitrary border width", async (t) => {
+  test("supports setting arbitrary border width", async ({ uno }) => {
     const classes = ["border-[66]", "border-[6]", "border-l-[7]", "border-r-[7]", "border-t-[7]", "border-b-[7]", "border-w-[7]"];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting arbitrary border colors", async (t) => {
+  test("supports setting arbitrary border colors", async ({ uno }) => {
     const classes = ["border-[--w-s-color-border]", "border-[var(--w-s-color-border)]"];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting arbitrary divide colors", async (t) => {
+  test("supports setting arbitrary divide colors", async ({ uno }) => {
     const classes = ["divide-[--w-s-color-border]", "divide-[var(--w-s-color-border)]"];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting border style", async (t) => {
+  test("supports setting border style", async ({ uno }) => {
     const classes = [
       "border-solid",
       "border-dashed",
@@ -62,24 +62,24 @@ describe("border", () => {
       "border-outset",
     ];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("supports setting border color", async (t) => {
+  test("supports setting border color", async ({ uno }) => {
     const classes = [
       "border-transparent",
       "border-inherit",
       "border-current",
     ];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
 
-  test("right, left, top bottom", async (t) => {
+  test("right, left, top bottom", async ({ uno }) => {
     const right = [
       "border-r",
       "border-r-0",
@@ -114,7 +114,7 @@ describe("border", () => {
 
     const classes = [...right, ...left, ...top, ...bottom];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
@@ -127,7 +127,7 @@ describe("border", () => {
   });
 
   test('supports divide borders between horizontal and stacked children in reverse order', async ({ uno }) => {
-    const classes = Object.keys(lineWidth).map(width => [`divide-x-reverse`, `divide-y-reverse`]).flat();
+    const classes = Object.keys(lineWidth).map(() => [`divide-x-reverse`, `divide-y-reverse`]).flat();
 
     const { css } = await uno.generate(classes);
     expect(css).toMatchSnapshot();
@@ -142,16 +142,16 @@ describe("border", () => {
     const classes = ['divide-x-10', 'divide-y-hej', 'divide-z-2-not-reverse'];
 
     const { css } = await uno.generate(classes);
-    expect(css).toMatchInlineSnapshot('""');
+    expect(css).toHaveLength(0);
   });
 
 });
 
 describe("rounded", () => {
-  test("supports x|y with value", async (t) => {
+  test("supports x|y with value", async ({ uno }) => {
     const classes = ["rounded", "rounded-0", "rounded-2", "rounded-4", "rounded-8", "rounded-16", "rounded-full", "rounded-t", "rounded-t-0", "rounded-t-2", "rounded-t-4", "rounded-t-8", "rounded-t-16", "rounded-t-full", "rounded-r", "rounded-r-0", "rounded-r-2", "rounded-r-4", "rounded-r-8", "rounded-r-16", "rounded-r-full", "rounded-b", "rounded-b-0", "rounded-b-2", "rounded-b-4", "rounded-b-8", "rounded-b-16", "rounded-b-full", "rounded-l", "rounded-l-0", "rounded-l-2", "rounded-l-4", "rounded-l-8", "rounded-l-16", "rounded-l-full", "rounded-tl-0", "rounded-tl-2", "rounded-tl-4", "rounded-tl-8", "rounded-tl-16", "rounded-tl-full", "rounded-tr-0", "rounded-tr-2", "rounded-tr-4", "rounded-tr-8", "rounded-tr-16", "rounded-tr-full", "rounded-br-0", "rounded-br-2", "rounded-br-4", "rounded-br-8", "rounded-br-16", "rounded-br-full", "rounded-bl-0", "rounded-bl-2", "rounded-bl-4", "rounded-bl-8", "rounded-bl-16", "rounded-bl-full"];
 
-    const { css } = await t.uno.generate(classes);
+    const { css } = await uno.generate(classes);
 
     expect(css).toMatchSnapshot();
   });
