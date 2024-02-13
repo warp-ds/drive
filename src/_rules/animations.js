a510407cce7743298d597e8a6a5e751d71b2e3bf
Magnus Hübner <magnus.hubner@aptitud.se>
2024-02-13 12:07:27 +0100

    fix: Remove use of deprecated component tokens

--- src/_rules/animations.js
+++ src/_rules/animations.js
@@ -1,12 +1,12 @@
 import { escapeSelector } from '@unocss/core';
 
 /*----LOADING----*/
-// Very hardcoded styling for loading, not decided if it will be themable so let's have this for now and change if applicable later
-const inProgress = 'background-image:linear-gradient(135deg,rgba(0, 0, 0, 0.05) 25%,transparent 0,transparent 50%,rgba(0, 0, 0, 0.05) 0,rgba(0, 0, 0, 0.05) 75%,transparent 0,transparent) !important;background-size: 30px 30px;animation: animate-inprogress 3s linear infinite;';
+// Very hardcoded styling for loading, not decided if it will be themeable so let's have this for now and change if applicable later
+const inProgress = 'background-image:linear-gradient(135deg,rgba(0,0,0,.05) 25%,transparent 0,transparent 50%,rgba(0,0,0,.05) 0,rgba(0,0,0,.05) 75%,transparent 0,transparent) !important;background-size: 30px 30px;animation: animate-inprogress 3s linear infinite;';
 const keyFrames = '@keyframes animate-inprogress {0% {background-position: 0 0;}to {background-position: 60px 0;}}';
 
 /*----SPINNER----*/
-const spinner = '--spinner-size: 24px;height: var(--spinner-size);width: var(--spinner-size);border-radius: 50%;border: calc(var(--spinner-size) / 8) solid var(--w-color-spinner-border);border-top-color: var(--w-s-color-border-primary);position: relative;animation: animate-spinner 0.75s infinite linear;';
+const spinner = '--spinner-size: 24px;height: var(--spinner-size);width: var(--spinner-size);border-radius: 50%;border: calc(var(--spinner-size) / 8) solid rgba(var(--w-s-rgb-border-primary),.5);border-top-color: var(--w-s-color-border-primary);position: relative;animation: animate-spinner 0.75s infinite linear;';
 const spinnerPseudo = `content: ' ';height: calc(var(--spinner-size) / 8);width: calc(var(--spinner-size) / 8);border-radius: 50%;position: absolute;top: calc(var(--spinner-size) / 180);`;
 const spinnerBefore = 'left:0;';
 const spinnerAfter = 'right:0;';
