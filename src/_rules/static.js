6ac0602ab75071e4e6543064427f8ff8f7ccea88
Magnus Hübner <magnus.hubner@aptitud.se>
2024-04-15 12:28:08 +0200

    feat: Added rules for break-before and break-after

--- src/_rules/static.js
+++ src/_rules/static.js
@@ -40,6 +40,22 @@ export const breaks = [
   ['break-inside-avoid', { 'break-inside': 'avoid' }],
   ['break-inside-avoid-page', { 'break-inside': 'avoid-page' }],
   ['break-inside-avoid-column', { 'break-inside': 'avoid-column' }],
+  ['break-before-auto', { 'break-before': 'auto' }],
+  ['break-before-avoid', { 'break-before': 'avoid' }],
+  ['break-before-all', { 'break-before': 'all' }],
+  ['break-before-avoid-page', { 'break-before': 'avoid-page' }],
+  ['break-before-page', { 'break-before': 'page' }],
+  ['break-before-left', { 'break-before': 'left' }],
+  ['break-before-right', { 'break-before': 'right' }],
+  ['break-before-column', { 'break-before': 'column' }],
+  ['break-after-auto', { 'break-after': 'auto' }],
+  ['break-after-avoid', { 'break-after': 'avoid' }],
+  ['break-after-all', { 'break-after': 'all' }],
+  ['break-after-avoid-page', { 'break-after': 'avoid-page' }],
+  ['break-after-page', { 'break-after': 'page' }],
+  ['break-after-left', { 'break-after': 'left' }],
+  ['break-after-right', { 'break-after': 'right' }],
+  ['break-after-column', { 'break-after': 'column' }],
 ];
 
 export const textOverflows = [
