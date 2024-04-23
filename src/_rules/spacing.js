0a869d38fac5a64cf04f031359dffee140da01b8
BalbinaK <balbuhhha@gmail.com>
2024-04-23 16:43:58 +0200

    fix: make arbitrary values more strict
    Accept only values starting with var(--, -- or a number

--- src/_rules/spacing.js
+++ src/_rules/spacing.js
@@ -6,28 +6,28 @@ export const padding = [
   [/^p()-(.\d*)$/, directionSize('padding'), { autocomplete: '(m|p)-<num>' }],
   [/^p([xy])-(.\d*)$/, directionSize('padding')],
   [/^p([rltb])-(.\d*)$/, directionSize('padding'), { autocomplete: '(m|p)<directions>-<num>' }],
-  // matching arbitrary values
+  // matching arbitrary values containing 'var(--)', '--', or a (negative) number with optional unit
   [
-    /^p-\[(.+)(rem|px)?]$/,
+    /^p-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/,
     ([, value, unit], context) => ({
       padding: resolveArbitraryValues(value, unit, context),
     }),
   ],
-  [/^p([xy])-\[(.+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`padding${i}`, resolveArbitraryValues(value, unit, context)])],
-  [/^p([rltb])-\[(.+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`padding${i}`, resolveArbitraryValues(value, unit, context)])],
+  [/^p([xy])-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`padding${i}`, resolveArbitraryValues(value, unit, context)])],
+  [/^p([rltb])-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`padding${i}`, resolveArbitraryValues(value, unit, context)])],
 ];
 
 export const margin = [
   [/^m()-(.+)$/, directionSize('margin')],
   [/^m([xy])-(.+)$/, directionSize('margin')],
   [/^m([rltb])-(.+)$/, directionSize('margin')],
-  // matching arbitrary values
+  // matching arbitrary values containing 'var(--)', '--', or a (negative) number with optional unit
   [
-    /^m-\[(.+)(rem|px)?]$/,
+    /^m-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/,
     ([, value, unit], context) => ({
       margin: resolveArbitraryValues(value, unit, context),
     }),
   ],
-  [/^m([xy])-\[(.+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
-  [/^m([rltb])-\[(.+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
+  [/^m([xy])-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
+  [/^m([rltb])-\[(var\(--.+\)|--.+|-?\d+)(rem|px)?]$/, ([, direction, value, unit], context) => directionMap[direction].map((i) => [`margin${i}`, resolveArbitraryValues(value, unit, context)])],
 ];
