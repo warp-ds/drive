51b4d5dd3be4511bf78021c59eb01ab40e09d800
BalbinaK <balbuhhha@gmail.com>
2024-12-13 13:16:37 +0100

    Fix lint issues

--- src/_rules/dataviz.js
+++ src/_rules/dataviz.js
@@ -1,52 +1,38 @@
-import { escapeSelector } from "@unocss/core";
+import { escapeSelector } from '@unocss/core';
 
-import { directionMap, handler as h } from "#utils";
+import { directionMap, handler as h } from '#utils';
 
-const handleBorder = ([, direction = "", semanticVal = ""]) =>
-  directionMap[direction.substring(1)]?.map((dir) => [
-    `border${dir}-color`,
-    h.datavizToken(`border${semanticVal}`),
-  ]);
+const handleBorder = ([, direction = '', semanticVal = '']) =>
+  directionMap[direction.substring(1)]?.map((dir) => [`border${dir}-color`, h.datavizToken(`border${semanticVal}`)]);
 
-const handleDivide = ([_selector, direction = "", semanticVal = ""]) =>
-  `.${escapeSelector(_selector)}>*+*{${directionMap[direction.substring(1)]?.map((dir) => `border${dir}-color: ${h.datavizToken(`border${semanticVal}`)};`).join("")}}`;
+const handleDivide = ([_selector, direction = '', semanticVal = '']) =>
+  `.${escapeSelector(_selector)}>*+*{${directionMap[direction.substring(1)]?.map((dir) => `border${dir}-color: ${h.datavizToken(`border${semanticVal}`)};`).join('')}}`;
 
 const getSemanticRegEx = (groupName, directions) =>
-  new RegExp(
-    `^dv-s-${groupName}${directions ? `(-[${directions}])?` : ""}((-[^\\/]+)?(\\/(0|[1-9][0-9]?|100))?)?$`
-  );
+  new RegExp(`^dv-s-${groupName}${directions ? `(-[${directions}])?` : ''}((-[^\\/]+)?(\\/(0|[1-9][0-9]?|100))?)?$`);
 
 export const datavizRules = [
   [
-    getSemanticRegEx("bg"),
-    ([, semanticVal = ""]) => ({
-      "background-color": h.datavizToken(`fill${semanticVal}`),
+    getSemanticRegEx('bg'),
+    ([, semanticVal = '']) => ({
+      'background-color': h.datavizToken(`fill${semanticVal}`),
     }),
   ],
+  [getSemanticRegEx('fill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`fill${semanticVal}`) })],
   [
-    getSemanticRegEx("fill"),
-    ([, semanticVal = ""]) => ({ fill: h.datavizToken(`fill${semanticVal}`) }),
-  ],
-  [
-    getSemanticRegEx("line"),
-    ([, semanticVal = ""]) => ({
+    getSemanticRegEx('line'),
+    ([, semanticVal = '']) => ({
       stroke: h.datavizToken(`line${semanticVal}`),
     }),
   ],
+  [getSemanticRegEx('text'), ([, semanticVal = '']) => ({ color: h.datavizToken(`text${semanticVal}`) })],
+  [getSemanticRegEx('icon'), ([, semanticVal = '']) => ({ color: h.datavizToken(`icon${semanticVal}`) })],
   [
-    getSemanticRegEx("text"),
-    ([, semanticVal = ""]) => ({ color: h.datavizToken(`text${semanticVal}`) }),
-  ],
-  [
-    getSemanticRegEx("icon"),
-    ([, semanticVal = ""]) => ({ color: h.datavizToken(`icon${semanticVal}`) }),
-  ],
-  [
-    getSemanticRegEx("outline"),
-    ([, semanticVal = ""]) => ({
-      "outline-color": h.datavizToken(`border${semanticVal}`),
+    getSemanticRegEx('outline'),
+    ([, semanticVal = '']) => ({
+      'outline-color': h.datavizToken(`border${semanticVal}`),
     }),
   ],
-  [getSemanticRegEx("border", "lrtbxy"), handleBorder],
-  [getSemanticRegEx("divide", "xy"), handleDivide],
+  [getSemanticRegEx('border', 'lrtbxy'), handleBorder],
+  [getSemanticRegEx('divide', 'xy'), handleDivide],
 ];
