eb3e67249a893daf3ccef9e2506a0d6990c3656c
BalbinaK <balbuhhha@gmail.com>
2024-12-17 13:30:27 +0100

    Use pattern with dv-s(-fill|stroke)?-category-semanticValue

--- src/_rules/dataviz.js
+++ src/_rules/dataviz.js
@@ -1,50 +1,22 @@
 import { directionMap, handler as h } from '#utils';
 
-const handleBorder = ([, direction = '', semanticVal = '']) =>
-  directionMap[direction.substring(1)]?.map((dir) => [`border${dir}-color`, h.datavizToken(`border${semanticVal}`)]);
+const handleBorder = ([, category, direction = '', semanticVal = '']) =>
+  directionMap[direction.substring(1)]?.map((dir) => [`border${dir}-color`, h.datavizToken(`${category}${semanticVal}`)]);
 
 const getSemanticRegEx = (groupName, directions) =>
   new RegExp(`^dv-s-${groupName}${directions ? `(-[${directions}])?` : ''}((-[^\\/]+)?(\\/(0|[1-9][0-9]?|100))?)?$`);
 
 export const datavizRules = [
-  // utilities pointing to background tokens
-  [getSemanticRegEx('svgfill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`background${semanticVal}`) })],
   [
-    getSemanticRegEx('bg'),
-    ([, semanticVal = '']) => ({
-      'background-color': h.datavizToken(`background${semanticVal}`),
+    /^dv-s-(fill|stroke)-(line|border|text|icon)((-[^\/]+)?(\/(0|[1-9][0-9]?|100))?)?$/,
+    ([, svgProperty, category, semanticVal = '']) => ({
+      [svgProperty]: h.datavizToken(`${category}${semanticVal}`),
     }),
   ],
-
-  // utilities pointing to line tokens
-  [
-    getSemanticRegEx('svgline'),
-    ([, semanticVal = '']) => ({
-      stroke: h.datavizToken(`line${semanticVal}`),
-    }),
-  ],
-
-  // utilities pointing to border tokens
-  [
-    getSemanticRegEx('svgborder'),
-    ([, semanticVal = '']) => ({
-      fill: h.datavizToken(`border${semanticVal}`),
-    }),
-  ],
-  [getSemanticRegEx('border', 'lrtbxy'), handleBorder],
-  [
-    getSemanticRegEx('outline'),
-    ([, semanticVal = '']) => ({
-      'outline-color': h.datavizToken(`border${semanticVal}`),
-    }),
-  ],
-
-  // utilities pointing to text tokens
-  [getSemanticRegEx('svgtext'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`text${semanticVal}`) })],
+  [/^dv-s-(line|border)(-[lrtbxy])?((-[^\/]+)?(\/(0|[1-9][0-9]?|100))?)?$/, handleBorder],
+  [getSemanticRegEx('fill-bg'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`background${semanticVal}`) })],
+  [getSemanticRegEx('bg'), ([, semanticVal = '']) => ({ 'background-color': h.datavizToken(`background${semanticVal}`) })],
+  [getSemanticRegEx('outline'), ([, semanticVal = '']) => ({ 'outline-color': h.datavizToken(`border${semanticVal}`) })],
   [getSemanticRegEx('text'), ([, semanticVal = '']) => ({ color: h.datavizToken(`text${semanticVal}`) })],
-
-  // utilities pointing to icon tokens
-  [getSemanticRegEx('svgiconfill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`icon${semanticVal}`) })],
-  [getSemanticRegEx('svgiconstroke'), ([, semanticVal = '']) => ({ stroke: h.datavizToken(`icon${semanticVal}`) })],
   [getSemanticRegEx('icon'), ([, semanticVal = '']) => ({ color: h.datavizToken(`icon${semanticVal}`) })],
 ];
