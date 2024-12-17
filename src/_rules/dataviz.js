ac911258e037cf39285668ba9edb7d594aeeb718
BalbinaK <balbuhhha@gmail.com>
2024-12-17 09:34:09 +0100

    Rename fill to background and handle svg & non-svg utilities

--- src/_rules/dataviz.js
+++ src/_rules/dataviz.js
@@ -1,38 +1,50 @@
-import { escapeSelector } from '@unocss/core';
-
 import { directionMap, handler as h } from '#utils';
 
 const handleBorder = ([, direction = '', semanticVal = '']) =>
   directionMap[direction.substring(1)]?.map((dir) => [`border${dir}-color`, h.datavizToken(`border${semanticVal}`)]);
 
-const handleDivide = ([_selector, direction = '', semanticVal = '']) =>
-  `.${escapeSelector(_selector)}>*+*{${directionMap[direction.substring(1)]?.map((dir) => `border${dir}-color: ${h.datavizToken(`border${semanticVal}`)};`).join('')}}`;
-
 const getSemanticRegEx = (groupName, directions) =>
   new RegExp(`^dv-s-${groupName}${directions ? `(-[${directions}])?` : ''}((-[^\\/]+)?(\\/(0|[1-9][0-9]?|100))?)?$`);
 
 export const datavizRules = [
+  // utilities pointing to background tokens
+  [getSemanticRegEx('svgfill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`background${semanticVal}`) })],
   [
     getSemanticRegEx('bg'),
     ([, semanticVal = '']) => ({
-      'background-color': h.datavizToken(`fill${semanticVal}`),
+      'background-color': h.datavizToken(`background${semanticVal}`),
     }),
   ],
-  [getSemanticRegEx('fill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`fill${semanticVal}`) })],
+
+  // utilities pointing to line tokens
   [
-    getSemanticRegEx('line'),
+    getSemanticRegEx('svgline'),
     ([, semanticVal = '']) => ({
       stroke: h.datavizToken(`line${semanticVal}`),
     }),
   ],
-  [getSemanticRegEx('text'), ([, semanticVal = '']) => ({ color: h.datavizToken(`text${semanticVal}`) })],
-  [getSemanticRegEx('icon'), ([, semanticVal = '']) => ({ color: h.datavizToken(`icon${semanticVal}`) })],
+
+  // utilities pointing to border tokens
+  [
+    getSemanticRegEx('svgborder'),
+    ([, semanticVal = '']) => ({
+      fill: h.datavizToken(`border${semanticVal}`),
+    }),
+  ],
+  [getSemanticRegEx('border', 'lrtbxy'), handleBorder],
   [
     getSemanticRegEx('outline'),
     ([, semanticVal = '']) => ({
       'outline-color': h.datavizToken(`border${semanticVal}`),
     }),
   ],
-  [getSemanticRegEx('border', 'lrtbxy'), handleBorder],
-  [getSemanticRegEx('divide', 'xy'), handleDivide],
+
+  // utilities pointing to text tokens
+  [getSemanticRegEx('svgtext'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`text${semanticVal}`) })],
+  [getSemanticRegEx('text'), ([, semanticVal = '']) => ({ color: h.datavizToken(`text${semanticVal}`) })],
+
+  // utilities pointing to icon tokens
+  [getSemanticRegEx('svgiconfill'), ([, semanticVal = '']) => ({ fill: h.datavizToken(`icon${semanticVal}`) })],
+  [getSemanticRegEx('svgiconstroke'), ([, semanticVal = '']) => ({ stroke: h.datavizToken(`icon${semanticVal}`) })],
+  [getSemanticRegEx('icon'), ([, semanticVal = '']) => ({ color: h.datavizToken(`icon${semanticVal}`) })],
 ];
--- test/dataviz.js
+++ test/dataviz.js
@@ -6,6 +6,8 @@ setup();
 
 test('it generates css based on semantic dataviz tokens', async ({ uno }) => {
   const classes = [
+    'dv-s-svgborder-warning',
+    'dv-s-svgline-warning',
     'dv-s-border-warning',
     'dv-s-border-l-warning',
     'dv-s-border-r-warning',
@@ -18,39 +20,27 @@ test('it generates css based on semantic dataviz tokens', async ({ uno }) => {
     'dv-s-outline-warning',
     'dv-s-outline-positive',
     'dv-s-outline-category6',
-    'dv-s-divide-neutral',
-    'dv-s-divide-neutral',
-    'dv-s-divide-x-neutral',
-    'dv-s-divide-y-neutral',
-    'dv-s-divide-category6',
-    'dv-s-fill-neutral',
-    'dv-s-fill-negative',
-    'dv-s-fill-warning',
-    'dv-s-fill-positive',
-    'dv-s-fill-category6',
+    'dv-s-svgfill-neutral',
     'dv-s-bg-neutral',
     'dv-s-bg-negative',
     'dv-s-bg-warning',
     'dv-s-bg-positive',
     'dv-s-bg-category6',
+    'dv-s-svgiconstroke-category7',
+    'dv-s-svgiconfill-category7',
     'dv-s-icon-category7',
     'dv-s-icon-category8',
     'dv-s-icon-negative',
     'dv-s-icon-neutral',
     'dv-s-icon-positive',
     'dv-s-icon-warning',
+    'dv-s-svgtext-category7',
     'dv-s-text-category7',
     'dv-s-text-category8',
     'dv-s-text-negative',
     'dv-s-text-neutral',
     'dv-s-text-positive',
     'dv-s-text-warning',
-    'dv-s-line-category7',
-    'dv-s-line-category8',
-    'dv-s-line-negative',
-    'dv-s-line-neutral',
-    'dv-s-line-positive',
-    'dv-s-line-warning',
   ];
   const antiClasses = [
     'dv-s-background',
@@ -59,7 +49,6 @@ test('it generates css based on semantic dataviz tokens', async ({ uno }) => {
     'dv-s-font-link-active',
     'dv-s-bordercolor',
     'dv-s-bordercolor-l-disabled',
-    'dv-s-dividecolor',
   ];
   const { css } = await uno.generate([...classes, ...antiClasses]);
   expect(css).toMatchSnapshot();
@@ -68,14 +57,16 @@ test('it generates css based on semantic dataviz tokens', async ({ uno }) => {
 test('it generates css based on semantic dataviz tokens with alpha channel', async ({ uno }) => {
   const classes = [
     'dv-s-border-warning/90',
-    'dv-s-fill-positive/100',
-    'dv-s-icon-category7/5',
+    'dv-s-svgborder-negative/90',
+    'dv-s-svgline-negative/90',
+    'dv-s-bg-positive/100',
+    'dv-s-svgfill-neutral/15',
+    'dv-s-outline-category7/5',
     'dv-s-icon-category8/99',
-    'dv-s-icon-negative/1',
-    'dv-s-icon-neutral/0',
-    'dv-s-icon-positive/4',
-    'dv-s-icon-warning/25',
-    'dv-s-text-neutral/15',
+    'dv-s-svgiconfill-negative/1',
+    'dv-s-svgiconstroke-neutral/0',
+    'dv-s-text-positive/4',
+    'dv-s-svgtext-warning/25',
   ];
   const { css } = await uno.generate(classes);
   expect(css).toMatchSnapshot();
@@ -103,16 +94,16 @@ test('it should not generate css with incorrect alpha channel values', async ({ 
 test('pseudo', async ({ uno }) => {
   const classes = [
     'focus:dv-s-icon-category7-focus',
-    'focus:dv-s-icon-category8-focus',
-    'focus:dv-s-icon-negative-focus',
+    'focus:dv-s-svgiconfill-category8-focus',
+    'focus:dv-s-svgiconstroke-negative-focus',
     'focus:dv-s-icon-neutral-focus',
     'focus:dv-s-icon-positive-focus',
     'focus:dv-s-icon-warning-focus',
     'hover:dv-s-border-primary-hover',
-    'hover:dv-s-fill-secondary-hover',
+    'hover:dv-s-bg-secondary-hover',
     'hover:dv-s-icon-negative-hover',
     'hover:dv-s-icon-neutral-hover',
-    'hover:dv-s-line-neutral-hover',
+    'hover:dv-s-svgline-neutral-hover',
     'hover:dv-s-text-positive-hover',
     'hover:dv-s-text-warning-hover',
   ];
